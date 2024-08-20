54b792576ae8e552471b2123b92e35e9ddd7995c
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-08-20 23:32:29 +0300

    new project

---
+++ Election_projectFor_me/Election_projectFor_me/Controllers/AdminController.cs
@@ -0,0 +1,112 @@
+﻿using Election_projectFor_me.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Election_projectFor_me.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ELECTION_PROJECTEntities _context;
+
+        public AdminController()
+        {
+            _context = new ELECTION_PROJECTEntities();
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var pendingAds = _context.Ads.Where(a => a.StatusOfAds == "pending").ToList();
+            return View(pendingAds);
+        }
+
+        // POST: Admin/Approve/5
+        [HttpPost]
+        public ActionResult Approve(int id, string adminComment)
+        {
+            var ad = _context.Ads.Find(id);
+            if (ad == null)
+            {
+                return HttpNotFound();
+            }
+
+            ad.StatusOfAds = "approved";
+            ad.AdminComment = adminComment;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: Admin/Reject/5
+        [HttpPost]
+        public ActionResult Reject(int id, string adminComment)
+        {
+            var ad = _context.Ads.Find(id);
+            if (ad == null)
+            {
+                return HttpNotFound();
+            }
+
+            ad.StatusOfAds = "rejected";
+            ad.AdminComment = adminComment;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult BulkAction(List<int> selectedIds, string action, FormCollection form)
+        {
+            if (selectedIds != null)
+            {
+                foreach (var id in selectedIds)
+                {
+                    var ad = _context.Ads.Find(id);
+                    if (ad != null)
+                    {
+                        if (action == "Approve")
+                        {
+                            ad.StatusOfAds = "approved";
+                        }
+                        else if (action == "Reject")
+                        {
+                            ad.StatusOfAds = "rejected";
+                        }
+
+                        // Retrieve admin comment from form data
+                        string adminCommentKey = $"adminComments[{id}]";
+                        if (form.AllKeys.Contains(adminCommentKey))
+                        {
+                            ad.AdminComment = form[adminCommentKey];
+                        }
+                    }
+                }
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult AddDate()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddDate([Bind(Include = "startDateNominate,EndDateNominate,startDateOfElection,EndDateOfElection")] DATE date)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.DATES.Add(date);
+                _context.SaveChanges();
+
+            }
+            return View();
+
+        }
+
+
+    }
+}
---
+++ Election_projectFor_me/Election_projectFor_me/Controllers/LocalListCandidatesController.cs
@@ -0,0 +1,159 @@
+﻿using Election_projectFor_me.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Election_projectFor_me.Controllers
+{
+    public class LocalListCandidatesController : Controller
+    {
+        private ELECTION_PROJECTEntities db = new ELECTION_PROJECTEntities();
+
+        // GET: LocalListCandidates
+        public ActionResult Index()
+        {
+            return View(db.LocalListCandidates.ToList());
+        }
+
+        // GET: LocalListCandidates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LocalListCandidate localListCandidate = db.LocalListCandidates.Find(id);
+            if (localListCandidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(localListCandidate);
+        }
+
+        // GET: LocalListCandidates/Create
+        public ActionResult Create(string listName)
+        {
+            ViewBag.ListName = listName;
+            return View(new List<LocalListCandidate>());
+        }
+
+        // POST: LocalListCandidates/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(List<LocalListCandidate> candidates, string ListName, string ElectionArea, string Governorate, string Image)
+        {
+
+            // إنشاء وحفظ قائمة جديدة
+            LocalList localList = new LocalList
+            {
+
+                ListName = ListName,
+                ElectionArea = ElectionArea,
+                Governorate = Governorate,
+                NumberOfVotes = 0,
+                img = Image
+
+            };
+
+            if (ModelState.IsValid)
+            {
+                if (localList.img != null)
+                {
+
+                    string path = Path.Combine(Server.MapPath("~/img"), localList.img);
+
+                }
+            }
+            db.LocalLists.Add(localList);
+            db.SaveChanges();
+
+            // حفظ بيانات المرشحين وربطهم بالقائمة
+            foreach (var candidate in candidates)
+            {
+                candidate.listname = localList.ListName; // الربط باستخدام معرف القائمة المحفوظة
+
+                db.LocalListCandidates.Add(candidate);
+            }
+            db.SaveChanges();
+
+            // إعادة توجيه المستخدم إلى صفحة النجاح أو صفحة أخرى
+            return RedirectToAction("index");
+        }
+
+
+        // POST: LocalListCandidates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CandidateID,CandidateName,NationalNumber,Governorate,ElectionArea,NumberOfVotesCandidate,LocalListingID,typeofCandidates")] LocalListCandidate localListCandidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(localListCandidate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(localListCandidate);
+        }
+
+        // GET: LocalListCandidates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LocalListCandidate localListCandidate = db.LocalListCandidates.Find(id);
+            if (localListCandidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(localListCandidate);
+        }
+
+        // POST: LocalListCandidates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LocalListCandidate localListCandidate = db.LocalListCandidates.Find(id);
+            db.LocalListCandidates.Remove(localListCandidate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public JsonResult GetUserData(string nationalNumber)
+        {
+            var user = db.USERS.FirstOrDefault(u => u.NationalNumber == nationalNumber);
+            if (user != null)
+            {
+                return Json(new
+                {
+                    FullName = user.FullName,
+                    ElectionArea = user.ElectionArea,
+                    Governorate = user.Governorate,
+                    Gender = user.Gender,
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(null, JsonRequestBehavior.AllowGet);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
